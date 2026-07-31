944994f4901792645915956bf2527ef04d30036b
Harry Keenan <keenanhf@outlook.com>
2026-07-30 22:31:57 -0500

    Unify the application workspace shell

--- modules/app_styles.py
+++ modules/app_styles.py
@@ -1,6 +1,7 @@
 from modules.design_tokens import DESIGN_TOKEN_CSS
 from modules.ui_primitive_styles import UI_PRIMITIVE_CSS
 from modules.ui_modal_styles import UI_MODAL_CSS
+from modules.application_shell_styles import APPLICATION_SHELL_CSS
 
 
 APP_CSS = """
@@ -10941,5 +10942,6 @@ div[class*="st-key-trade_hub_"] [data-testid="stExpander"] {
     }
 }
 
+""" + APPLICATION_SHELL_CSS + """
 </style>
 """
